de004098df04bf07a7ddad7dc885bc30d2931539
suryakanta-mohanty <suryakantamohanty855@gmail.com>
2024-06-04 00:16:11 +0530

    save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) || [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2, 
 },
@@ -6,6 +6,10 @@ export let cart = [{
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity: 1
 }];
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId){
   let matchingItem;
@@ -31,6 +35,8 @@ export function addToCart(productId){
       quantity
     });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -43,4 +49,6 @@ export function removeFromCart(productId){
   });
 
   cart = newCart;
+
+  saveToStorage();
 }
