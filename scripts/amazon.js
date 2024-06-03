caeb935038bc4441b5213f287330bbdb0082ceff
suryakanta-mohanty <suryakantamohanty855@gmail.com>
2024-06-03 21:02:01 +0530

    adding a code that will show message when the add-to-cart button will be press.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,55 +1,9 @@
 
-
 /* the main idea of javascipt
    1. Save the data
    2. Generate the HTML
    3. Make it interactive 
 */
-
-
-// save data - data structure.
-
-/*
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090
-},
-
-{
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4.0,
-    count: 127
-  },
-  priceCents: 2095
-},
-
-{
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799
-},
-
-{
-  image: 'images/products/black-2-slot-toaster.jpg',
-  name: '2 Slot Toaster - Black',
-  rating: {
-    stars: 5.0,
-    count: 2197
-  },
-  priceCents: 1899
-}];
-*/ 
 
 // Generate the HTML
 // combine this html together.
@@ -97,7 +51,7 @@ products.forEach((product) =>{
 
     <div class="product-spacer"></div>
 
-    <div class="added-to-cart">
+    <div class="added-to-cart js-added-to-cart-${product.id}">
       <img src="images/icons/checkmark.png">
       Added
     </div>
@@ -119,8 +73,6 @@ products.forEach((product) =>{
        - then give it any name (example data-product-name)
 */
 
-
-
 // put it on the web page
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
@@ -132,6 +84,8 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
    2. allows us to attach any information to an element. 
    - see the example in "add to cart button"
 */
+
+const addedMessageTimeouts = {};
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) =>{
   button.addEventListener('click', ()=>{
@@ -148,7 +102,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) =>{
 
     // const productId = button.dataset.productId;
     const {productId} = button.dataset;   // destructuring
-
+    
     let matchingItem;
     cart.forEach((item) =>{
       if(productId === item.productId){
@@ -181,8 +135,22 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) =>{
 
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 
+    const addedMessage =  document.querySelector(`.js-added-to-cart-${productId}`);
+    addedMessage.classList.add('added-to-cart-visible');
+
+  
+    const previousTimeoutId = addedMessageTimeouts[productId];
+
+    if(previousTimeoutId){
+      clearTimeout(previousTimeoutId);
+    }
     
+
+    const timeoutId = setTimeout(() => {
+      addedMessage.classList.remove('added-to-cart-visible');
+    }, 2000);
+
+    addedMessageTimeouts[productId] = timeoutId;
   });
 
 });
-
