e0875059f5a512cec9a3c1df5c269889b045f581
suryakanta-mohanty <suryakantamohanty855@gmail.com>
2024-06-04 21:39:39 +0530

    learning how to add external js file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,10 @@ import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
+
+hello();
+console.log(dayjs());
+
 let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => {
