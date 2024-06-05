9adc7a056966a69ed68daae80d42bf58a6e2fb73
suryakanta-mohanty <suryakantamohanty855@gmail.com>
2024-06-05 10:46:47 +0530

    adding code for cart quantity so that the cart quantity can show in both both. And reducing the repated code.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js"; 
+import { cart, removeFromCart, calculateCartQuantity } from "../data/cart.js"; 
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -112,5 +112,15 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
 
     const container = document.querySelector(`.js-cart-item-container-${productId}`);
     container.remove();
+
+    updateCartQuantity();
   });
 });
+
+function updateCartQuantity(){
+  const cartQuantity = calculateCartQuantity();
+
+  document.querySelector('.js-return-to-home-link').innerHTML = `${cartQuantity} Items`;
+}
+
+updateCartQuantity(); 
