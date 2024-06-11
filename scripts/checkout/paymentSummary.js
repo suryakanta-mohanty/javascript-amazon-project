895fbb9fd3c9fa49aa19dac49ce16a16100e730b
suryakanta-mohanty <suryakantamohanty855@gmail.com>
2024-06-11 11:32:52 +0530

    create order for backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import {cart} from '../../data/cart.js';
 import { getProduct } from '../../data/products.js';
 import { getDeliveryOption } from '../../data/deliveryOption.js';
 import {formatCurrency} from '../utils/money.js';
+import {addOrders} from '../../data/orders.js' ;
 
 export function renderPaymentSummary(){ 
 
@@ -60,11 +61,33 @@ export function renderPaymentSummary(){
         <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
       </div>
 
-      <button class="place-order-button button-primary">
+      <button class="place-order-button button-primary js-place-order">
         Place your order
       </button>
   
    `;
 
    document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+
+   document.querySelector('.js-place-order').addEventListener('click', async () =>{
+    try {
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST',
+        header:{
+          'content-type': 'application/json'
+        },
+        body : JSON.stringify({
+          cart : cart
+        })
+      });
+  
+      const order = await response.json();
+      addOrders(order);
+
+    } catch (error) {
+      console.log('Unexpected Error. Try again later.');
+    }
+    
+    window.location.href = 'orders.html';
+   });
 }
