34003d695bf42cc2fdcbca21b1a9ae9241e01429
suryakanta-mohanty <suryakantamohanty855@gmail.com>
2024-07-25 16:45:38 +0530

    renaming the main page

--- scripts/checkout/checkoutHeader.js
+++ scripts/checkout/checkoutHeader.js
@@ -11,7 +11,7 @@ export function renderCheckoutHeader(){
   const checkoutHeaderHTML = `
       <div class="header-content">
         <div class="checkout-header-left-section">
-          <a href="amazon.html">
+          <a href="index.html">
             <img class="amazon-logo" src="images/amazon-logo.png">
             <img class="amazon-mobile-logo" src="images/amazon-mobile-logo.png">
           </a>
@@ -19,7 +19,7 @@ export function renderCheckoutHeader(){
 
         <div class="checkout-header-middle-section">
           Checkout (<a class="return-to-home-link js-return-to-home-link"
-            href="amazon.html">${cartQuantity}</a>)
+            href="index.html">${cartQuantity}</a>)
         </div>
 
         <div class="checkout-header-right-section">
